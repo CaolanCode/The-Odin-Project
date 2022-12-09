6e49eb0120021c2f28e5464b50bc28129a28a04a
CaolanCode <cmacmahon3@gmail.com>
2022-12-09 14:20:05 +0000

    commit play round function

--- JavaScript/RockPaperScissors/rock-paper-scissors.js
+++ JavaScript/RockPaperScissors/rock-paper-scissors.js
@@ -1,5 +1,40 @@
 function getComputerChoice(){
-    let allChoices = ['Rock', 'Paper', 'Scissors']
+    let allChoices = ['rock', 'paper', 'scissors']
     let choice = allChoices[Math.floor(Math.random()*allChoices.length)]
     return choice; 
+}
+
+function playRound(playerChoice, computerChoice){
+
+    switch(computerChoice){
+        case 'rock':
+            if(playerChoice == 'rock'){
+                return "It is a draw";
+            } else if(playerChoice == 'scissors'){
+                return "You won!";
+            } else if(computerChoice == 'paper'){
+                return "You lost";
+            }
+            break;
+        case 'paper':
+            if(playerChoice == 'rock'){
+                return "You won!";
+            } else if(playerChoice == 'scissors'){
+                return "You lost";
+            } else if(computerChoice == 'paper'){
+                return "It is a draw";
+            }
+           break;
+        case 'scissors':
+            if(playerChoice == 'rock'){
+                return "You lost";
+            } else if(playerChoice == 'scissors'){
+                return "It is a draw";
+            } else if(computerChoice == 'paper'){
+                return "You won!";
+            }
+           break;
+        default:
+            return "Incorrect input";
+    }
 }
