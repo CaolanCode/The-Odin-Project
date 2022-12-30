d6a463964362bf2e52677838dcd11a36ff06dc63
CaolanCode <cmacmahon3@gmail.com>
2022-12-30 19:39:50 +0000

    commit arrow function with parameter

--- JavaScript/ArrowFunctions/app.js
+++ JavaScript/ArrowFunctions/app.js
@@ -1,4 +1,10 @@
 const sayHello =  () => {
   return "Hello"
 }
-console.log(sayHello)
+console.log(sayHello())
+
+const sayHelloAgain = (name) => {
+  return `Hello , I'm ${name}`
+}
+const ben = sayHelloAgain('Ben')
+console.log(ben)
